47483713c8d7960063ea50062407b87d99ab3779
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-11-06 16:31:36 +0100

    Merge

--- controller/recipe.js
+++ controller/recipe.js
@@ -1,8 +1,36 @@
 import {executeQuery} from '../config/db';
 
 const getAllRecipes= async (req,res)=>{
-    let recipeData = await executeQuery('select * from recipe',[])
+   try{ let recipeData = await executeQuery('select * from recipe',[])
     res.send(recipeData)
 }
+    catch (error){   
+        res.status(500).json(error)
+            }
+}
 
-export {getAllRecipes};
+const deleteRecipebyId = async (req,res)=>{
+    let id=req.query.id
+    try{let recipeData = await executeQuery('DELETE FROM recipe where recipe_id=?}',[id])
+    res.send(recipeData)}
+    catch (error){   
+        res.status(500).json(error)
+            }
+}
+const updateRecipebyId = async (req,res)=>{
+    let id=req.query.id
+    const {recipeTitle,created_at,content,imgUrl}=req.body;
+    try{let recipeData = await executeQuery('select * FROM recipe where id=?}',[id]);
+    if (recipeData.length>0){
+        recipeData = await executeQuery ('update recipe set recipeTitle=?,created_at=?,content=?,img_Url=?',[id])  
+    } else {
+res.status(400).json(`recipe is not found in the id=${id}`);
+    }
+    res.send(recipeData)}
+    catch (error){   
+        res.status(500).json(error)
+            }
+}
+
+
+export {getAllRecipes,deleteRecipebyId,updateRecipebyId};
--- pages/api/recipe/recipe.js
+++ pages/api/recipe/recipe.js
@@ -1,6 +1,8 @@
 import nc from "next-connect";
-import {getAllRecipes, recipe} from '../../../controller/recipe' 
+import {getAllRecipes, recipe,deleteRecipebyId,updateRecipebyId} from '../../../controller/recipe';
+
 const  handler = nc();
 handler.get(getAllRecipes)
-
+handler.delete(deleteRecipebyId)
+handler.put(updateRecipebyId)
 export default handler;
